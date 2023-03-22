5e5a92417041b4ef1c1eac3568d081dd4b02bafd
Olesya <53311515+OlesyaU@users.noreply.github.com>
2023-03-22 11:29:31 +0300

    Приложение для отображения контента в Documents 1. ✅При старте приложения показывается контент директории Documents. В навигейшн баре есть одна кнопка: * Добавить фотографию 2. ✅При нажатии на кнопку Добавить фотографию показывается Image Picker. При выборе фотографии она сохраняется в Documents и должна отобразиться в списке на экране. 3. ✅3* (со звездочкой). При свайпе объекта списка отображать кнопку Удалить, при нажатии на которую должно производиться соответствующее действие.

--- ImagePicker Bundle. Sandbox. FileManager/ImageManager.swift
+++ ImagePicker Bundle. Sandbox. FileManager/ImageManager.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class ImageManager: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     private  var imagePecker = UIImagePickerController()
     static let defaultManager = ImageManager()
