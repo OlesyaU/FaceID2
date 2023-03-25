493743c65a2779b652a9f16dba129fc90934b590
Olesya <53311515+OlesyaU@users.noreply.github.com>
2023-03-25 17:53:49 +0300

    3. ✅Если у пользователя есть сохранённый пароль, при нажатии на кнопку нужно проверить сохранённый пароль. Если он совпадает, открывается экран из пункта 4. Если нет, показывается соответствующая ошибка. Пока не на тот экран прееход

--- ImagePicker Bundle. Sandbox. FileManager/LoginController.swift
+++ ImagePicker Bundle. Sandbox. FileManager/LoginController.swift
@@ -19,19 +19,18 @@ class LoginController: UIViewController {
     private var arrayPasswords = [String]()
     @IBOutlet weak var stateSwitch: UISwitch!
     @IBOutlet weak var stateLabel: UILabel!
+    
     enum State {
         case newUser
         case knownUser
     }
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        print(counter)
         print(keyChain.allKeys)
-        print(arrayPasswords)
         setState(state: .newUser)
-allPass()
-
+        allPass()
+        
 //        keyChain.clear()
     }
     
@@ -46,7 +45,7 @@ allPass()
     
     @IBAction func switchAction(_ sender: Any) {
         if stateSwitch.isOn {
- setState(state: .knownUser)
+            setState(state: .knownUser)
         } else {
             setState(state: .newUser)
         }
@@ -56,72 +55,55 @@ allPass()
         guard let pass1 = passTextField.text else {
             return
         }
-
-       
-       print(arrayPasswords)
         
-        print(" first key \(key)")
         if   !stateSwitch.isOn, counter == 0  {
-
             key = String(Float.random(in: 0.00...100.00))
             counter += 1
-            print("counter = \(counter)")
-                        keyChain.set(pass1, forKey: String(key))
-                        arrayPasswords.append(pass1)
-            print(pass1,arrayPasswords, key)
+            keyChain.set(pass1, forKey: String(key))
+            arrayPasswords.append(pass1)
             button.setTitle("Повторите пароль", for: .normal)
             stateLabel.text = "Необходимо ввести пароль ещё раз"
             passTextField.text = nil
-            print(" second in if block where creat  key \(key)")
-
-        } else if  comparePasswords(pass1) ,(counter >= 1){
+        } else if  comparePasswords(pass1) ,(counter >= 1)  || stateSwitch.isOn {
             isLogin = true
-                       navigationController?.pushViewController(TableController(), animated: true)
-           
-          
-        }
-
-        else  if pass1.count < 4 {
+            navigationController?.pushViewController(TableController(), animated: true)
+        } else  if pass1.count < 4 {
             counter = 0
             passTextField.text = nil
-            
             LoginManager.showLoginAleart(in: self, with: "Ваш пароль короткий. Необходимо ввести более 4 символов")
             setState(state: .newUser)
-            
         } else if  !comparePasswords(pass1), counter >= 1 {
-
             keyChain.delete(String(key))
             passTextField.text = nil
-counter = 0
-
             LoginManager.showLoginAleart(in: self, with: "Пароль не совпадает попробуй ещё")
-//            setState(state: .newUser)
-            print(arrayPasswords)
+            setState(state: .newUser)
         }
     }
+    
     func comparePasswords(_ pass: String) -> Bool{
         arrayPasswords.contains(pass)
     }
+    
     func allPass(){
-
-      keyChain.allKeys.forEach({ key  in
-          let val = keyChain.get(key)
-          arrayPasswords.append(val!)
+        keyChain.allKeys.forEach({ key  in
+            let val = keyChain.get(key)
+            arrayPasswords.append(val!)
         })
         print(arrayPasswords)
     }
-//    llllll
+    
     private func setState(state: State) {
-     switch state {
+        switch state {
             case .newUser:
                 stateLabel.text = "Я новенький. Хочу сохраниться"
                 button.setTitle("Создать пароль", for: .normal)
-             stateSwitch.isOn = false
-             counter = 0
+                stateSwitch.isOn = false
+                passTextField.text = nil
+                counter = 0
             case .knownUser:
                 stateLabel.text = "У меня есть пароль"
                 button.setTitle("Ввести пароль", for: .normal)
-             stateSwitch.isOn = true
+                stateSwitch.isOn = true
         }
     }
 }
